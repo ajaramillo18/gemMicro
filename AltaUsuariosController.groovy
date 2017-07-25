dddff8d42d9c9cd3214fae9fcd1bbc18e87cb362
ajaramillo18 <ajaramillo18@hotmail.com>
2017-07-24 21:50:47 -0500

    Add files via upload

---
+++ AltaUsuariosController.groovy
@@ -0,0 +1,20 @@
+package genmicro
+
+class AltaUsuariosController {
+
+	def altaUsuariosService
+    def index = { redirect(action:iniciaCarga)}
+	def iniciaCarga =	{
+				
+	}
+	def altaUsuarios = {
+		
+		
+		def resultado
+		resultado = altaUsuariosService.alta(params.csvFile, params.micrositio)
+		
+		redirect(action:'iniciaCarga', params: [resultado: "${resultado}"])
+		
+	}
+	
+}
